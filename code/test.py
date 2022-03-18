81adb07b6edf28fc2cc5c78d7f14013da94f9ed1
AJArnolie <ajarno@stanford.edu>
2022-03-17 17:41:59 -0700

    Slight adjustments to source code to support easier testing

--- code/test.py
+++ code/test.py
@@ -6,7 +6,7 @@ import os
 import cv2
 import numpy as np
 from collections import OrderedDict
-
+import torchvision.transforms as transforms
 import torch
 from torch.utils.data import DataLoader
 import torch.backends.cudnn as cudnn
@@ -34,15 +34,10 @@ def main():
         device = torch.device('cpu')
 
     if args.save_eval_pngs or args.save_visualize:
-        result_path = os.path.join(args.result_dir, args.exp_name)
+        result_path = os.path.join(args.data_path, "GLP_depth")
         logging.check_and_make_dirs(result_path)
         print("Saving result images in to %s" % result_path)
     
-    if args.do_evaluate:
-        result_metrics = {}
-        for metric in metric_name:
-            result_metrics[metric] = 0.0
-
     print("\n1. Define Model")
     model = GLPDepth(max_depth=args.max_depth, is_train=False).to(device)
     model_weight = torch.load(args.ckpt_dir)
@@ -51,64 +46,45 @@ def main():
     model.load_state_dict(model_weight)
     model.eval()
 
-    print("\n2. Define Dataloader")
-    if args.dataset == 'imagepath': # not for do_evaluate in case of imagepath
-        dataset_kwargs = {'dataset_name': 'ImagePath', 'data_path': args.data_path}
-    else:
-        dataset_kwargs = {'data_path': args.data_path, 'dataset_name': args.dataset,
-                          'is_train': False}
-
-    test_dataset = get_dataset(**dataset_kwargs)
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False,
-                             pin_memory=True)
-
     print("\n3. Inference & Evaluate")
-    for batch_idx, batch in enumerate(test_loader):
-        input_RGB = batch['image'].to(device)
-        filename = batch['filename']
-
+    path = os.path.join(args.data_path, "downsampled_images") # the dir of imgs
+    imgs_list = os.listdir(path)
+    transform = transforms.ToTensor()
+    for idx, i in enumerate(imgs_list):
+        print(os.path.join(path, i))
         with torch.no_grad():
-            pred = model(input_RGB)
+            img = cv2.imread(os.path.join(path, i))
+            img_resize = cv2.resize(img, (int(img.shape[1]), int(img.shape[0])), interpolation=cv2.INTER_LINEAR)
+            img_torch = transform(img_resize).cuda()
+            img_torch = img_torch[None, :, :, :].cuda()
+            pred = model(img_torch)
         pred_d = pred['pred_d']
-
-        if args.do_evaluate:
-            depth_gt = batch['depth'].to(device)
-            pred_d, depth_gt = pred_d.squeeze(), depth_gt.squeeze()
-            pred_crop, gt_crop = metrics.cropping_img(args, pred_d, depth_gt)
-            computed_result = metrics.eval_depth(pred_crop, gt_crop)
-            for metric in metric_name:
-                result_metrics[metric] += computed_result[metric]
-
+       
         if args.save_eval_pngs:
-            save_path = os.path.join(result_path, filename[0])
+            save_path = os.path.join(result_path, i)
             if save_path.split('.')[-1] == 'jpg':
                 save_path = save_path.replace('jpg', 'png')
             pred_d = pred_d.squeeze()
-            if args.dataset == 'nyudepthv2':
-                pred_d = pred_d.cpu().numpy() * 1000.0
-                cv2.imwrite(save_path, pred_d.astype(np.uint16),
-                            [cv2.IMWRITE_PNG_COMPRESSION, 0])
-            else:
-                pred_d = pred_d.cpu().numpy() * 256.0
-                cv2.imwrite(save_path, pred_d.astype(np.uint16),
+            pred_d = pred_d.cpu().numpy() * 800 #256.0
+            cv2.imwrite(save_path, pred_d.astype(np.uint16),
                             [cv2.IMWRITE_PNG_COMPRESSION, 0])
             
-        if args.save_visualize:
-            save_path = os.path.join(result_path, filename[0])
-            pred_d_numpy = pred_d.squeeze().cpu().numpy()
-            pred_d_numpy = (pred_d_numpy / pred_d_numpy.max()) * 255
-            pred_d_numpy = pred_d_numpy.astype(np.uint8)
-            pred_d_color = cv2.applyColorMap(pred_d_numpy, cv2.COLORMAP_RAINBOW)
-            cv2.imwrite(save_path, pred_d_color)
-        logging.progress_bar(batch_idx, len(test_loader), 1, 1)
+        #if args.save_visualize:
+        #    save_path = os.path.join(result_path, i)
+        #    pred_d_numpy = pred_d.squeeze().cpu().numpy()
+        #    pred_d_numpy = (pred_d_numpy / pred_d_numpy.max()) * 255
+        #    pred_d_numpy = pred_d_numpy.astype(np.uint8)
+        #    pred_d_color = cv2.applyColorMap(pred_d_numpy, cv2.COLORMAP_RAINBOW)
+        #    cv2.imwrite(save_path, pred_d_color)
+        logging.progress_bar(idx, len(imgs_list), 1, 1)
 
-    if args.do_evaluate:
-        for key in result_metrics.keys():
-            result_metrics[key] = result_metrics[key] / (batch_idx + 1)
-        display_result = logging.display_result(result_metrics)
-        if args.kitti_crop:
-            print("\nCrop Method: ", args.kitti_crop)
-        print(display_result)
+    #if args.do_evaluate:
+    #    for key in result_metrics.keys():
+    #        result_metrics[key] = result_metrics[key] / (batch_idx + 1)
+    #    display_result = logging.display_result(result_metrics)
+    #    if args.kitti_crop:
+    #        print("\nCrop Method: ", args.kitti_crop)
+    #    print(display_result)
 
     print("Done")
 
